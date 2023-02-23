575eea0450259e68d091978015973f31656bd4d7
Xavier Julian Rodriguez <106761585+Xavier-Julian-Rodriguez@users.noreply.github.com>
2023-02-23 16:19:02 -0500

    init commit

---
+++ src/components/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Header from "./Header";
+import Note from "./Note";
+import Footer from "./Footer";
+import "./Styles.css";
+
+const App = () => {
+    return (
+        <div>
+            <Header />
+            <Note />
+            <Footer />
+        </div>
+    )
+}
+
+export default App;
